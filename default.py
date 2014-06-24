7a7ed669a010e9896c38c645bcfae05e103cd750
bromix <bromix@gmx.net>
2014-06-24 16:14:43 +0200

    list highlights implemented

--- default.py
+++ default.py
@@ -4,6 +4,8 @@ import xbmcgui
 import xbmcaddon
 
 import os
+import json
+import urllib2
 
 import pydevd
 pydevd.settrace('localhost', stdoutToServer=True, stderrToServer=True)
@@ -19,8 +21,46 @@ __FANART__ = os.path.join(bromixbmc.Addon.Path, "fanart.jpg")
 
 ACTION_SHOW_HIGHLIGHTS = 'showHighlights'
 ACTION_SHOW_LIBRARY = 'showLibrary'
+ACTION_PLAY = 'play'
 
 SETTING_SHOW_FANART = bromixbmc.Addon.getSetting('showFanart')=="true"
+
+def _getContentAsJson(url):
+    result = {}
+    
+    try:
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-Agent', 'stagefright/1.2 (Linux;Android 4.4.2)')]
+        
+        content = opener.open(url)
+        result = json.load(content)
+    except:
+        # do nothing
+        pass
+    
+    return result
+
+def _listEpisodes(episodes):
+    episodes_list = episodes.get('episodes-list', {})
+    
+    for episode in episodes_list:
+        thumbnailImage = episode.get('episode-image', "")
+        title = episode.get('episode-title', "")
+        subtitle = episode.get('episode-subtitle', "")
+        id = str(episode.get('episode-id', ""))
+        
+        name = title+" - "+subtitle
+        
+        if id!="":
+            params = {'action': ACTION_PLAY,
+                      'episode': id}
+            bromixbmc.addVideoLink(name, params=params, thumbnailImage=thumbnailImage, fanart=__FANART__)
+        pass
+
+def _listJsonResult(jsonResult):
+    episodes = jsonResult.get('episodes', None)
+    if episodes!=None:
+        _listEpisodes(episodes)
 
 def showIndex():
     # add 'Highlights'
@@ -34,4 +74,17 @@ def showIndex():
     xbmcplugin.endOfDirectory(bromixbmc.Addon.Handle)
     return True
 
-showIndex()
+def showHighlights():
+    json = _getContentAsJson('http://m.app.dmax.de/free-to-air/android/genesis/targets/featured/')
+    
+    _listJsonResult(json)
+    
+    xbmcplugin.endOfDirectory(bromixbmc.Addon.Handle)
+    return True
+
+action = bromixbmc.getParam('action')
+
+if action==ACTION_SHOW_HIGHLIGHTS:
+    showHighlights()
+else:
+    showIndex()
